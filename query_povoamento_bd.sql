9ccf3642e540971fa52c72920c27b69b73e2c6e7
victorhugo457 <victorhugofigueiredorios1563@gmail.com>
2024-11-30 10:15:23 -0300

    Add files via upload

---
+++ query_povoamento_bd.sql
@@ -0,0 +1,34 @@
+use biblioteca;
+insert into genero (genero)
+values('Fantasia'),
+('Romance'),
+('Ficção Científica'),
+('Suspense'),
+('Mistério'),
+('Biografia'),
+('Drama'),
+('Auto Ajuda'),
+('Poesia'),
+('História');
+
+insert into usuario (username, password, role)
+values ('bibliotecaria', '$2a$10$MfU7xkIVRGZWILOdDMGcd./vgvq.SHf91Scremd1u6gc1Hb8daNlG', 'USER'),
+('user', '$2a$10$qaHDTkoFArsZRbPXchiSbez2KL2M9UEZYL5UJ5UyXBvHnSFICeAkO', 'USER');
+
+insert into autor(nome)
+values ('J. L. Rowling'),
+('J. R. R. Tolkien'),
+('Jane Austen'),
+('Georg Orwell'),
+('Willian P. Young'),
+('Machado de Assis'),
+('Antonie de Saint-Exupéry'),
+('Fiodor Dastoiévski'),
+('Markus Zusak'),
+('Paulo Coelho'),
+('Victória Aveyard'),
+('Sarah J. Maas'),
+('V. E. Schwab'),
+('Angie Thomas'),
+('Neal Schusterman');
+
